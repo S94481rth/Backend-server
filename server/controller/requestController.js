6f1f663899b7287f4aa93ff7cf21f3a4256233b0
Aravind <aravind.cs20@bmsce.ac.in>
2024-03-01 23:03:50 +0530

    changed console msg from server name to port

--- server/controller/requestController.js
+++ server/controller/requestController.js
@@ -2,7 +2,8 @@ const express = require("express")
 require("dotenv").config()
 const SERVER_NAME = process.env.SERVER_NAME
 const IP_ADDRESS = require("../utilities/ipAddress").ipaddress()
+const PORT = process.env.PORT
 
 exports.welcome = (req,res) => {
-    res.end(`response from the server whose name is ${SERVER_NAME} and ip address is ${IP_ADDRESS}`)
+    res.end(`response from the server whose port is ${PORT} and ip address is ${IP_ADDRESS}`)
 }
